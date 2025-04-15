08005fabd6820184a5d8de3c1f56b84cb55b2583
Abhinaykotla <abhinaykotla@gmail.com>
2025-04-15 03:57:34 -0500

    UPDATE: refine configuration parameters for dataset paths, batch size, and model architecture

--- config.py
+++ config.py
@@ -7,10 +7,12 @@ class Config:
     DATASET_PATH = os.path.join('data', 'intel-image-classification')
     TRAIN_DATASET_PATH = os.path.join(DATASET_PATH, 'seg_train', 'seg_train')
     TEST_DATASET_PATH = os.path.join(DATASET_PATH, 'seg_test', 'seg_test')
+    TEACHER_MODEL_PATH = './checkpoints/teacher_32layers_model.pth'
+
 
     # Hyperparameters
-    BATCH_SIZE = 128
-    NUM_WORKERS = 12
+    BATCH_SIZE = 32
+    NUM_WORKERS = 6
     LEARNING_RATE = 0.001
     MAX_EPOCHS = 15
     PATIENCE = 3
@@ -18,8 +20,8 @@ class Config:
     
     # Model configuration
     NUM_CLASSES = 6
-    RESIDUAL_BLOCKS = 8  # Number of residual blocks
-    FULLY_CONNECTED_LAYERS = [1024, 256, 64]  # List of sizes for fully connected layers
+    RESIDUAL_BLOCKS = 2  # Default number of residual blocks
+    FULLY_CONNECTED_LAYERS = [512]  # List of sizes for fully connected layers
 
     # Device configuration
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
