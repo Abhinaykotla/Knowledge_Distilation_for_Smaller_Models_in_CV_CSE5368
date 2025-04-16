306062343b6f0ce70d1aa36fc424057e3798b49d
Abhinaykotla <abhinaykotla@gmail.com>
2025-04-16 13:55:17 -0500

    ADD: implement additional CNN architectures (CustomSceneCNN6, CustomSceneCNN8, CustomSceneCNN10) with enhanced layers and dropout for improved performance

--- models/cnn_model.py
+++ models/cnn_model.py
@@ -55,3 +55,143 @@ class CustomSceneCNN(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.fc_layers(x)
         return x
+
+class CustomSceneCNN6(nn.Module):
+    def __init__(self):
+        super(CustomSceneCNN6, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.layer1 = ResidualBlock(16, 16)
+        self.layer2 = ResidualBlock(16, 32, stride=2)
+        self.layer3 = ResidualBlock(32, 32)
+        self.layer4 = ResidualBlock(32, 64, stride=2)
+        self.layer5 = ResidualBlock(64, 64)
+        self.layer6 = ResidualBlock(64, 128, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.fc1 = nn.Linear(128, 64)
+        self.fc_bn1 = nn.BatchNorm1d(64)
+        self.fc2 = nn.Linear(64, 6)
+
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.relu(self.bn1(self.conv1(x)))
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+
+        x = self.relu(self.fc_bn1(self.fc1(x)))
+        x = self.dropout(x)
+        x = self.fc2(x)
+
+        return x
+    
+
+class CustomSceneCNN8(nn.Module):
+    def __init__(self):
+        super(CustomSceneCNN8, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.layer1 = ResidualBlock(16, 16)
+        self.layer2 = ResidualBlock(16, 32, stride=2)
+        self.layer3 = ResidualBlock(32, 32)
+        self.layer4 = ResidualBlock(32, 64, stride=2)
+        self.layer5 = ResidualBlock(64, 64)
+        self.layer6 = ResidualBlock(64, 128, stride=2)
+        self.layer7 = ResidualBlock(128, 128)
+        self.layer8 = ResidualBlock(128, 256, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.fc1 = nn.Linear(256, 128)
+        self.fc_bn1 = nn.BatchNorm1d(128)
+        self.fc2 = nn.Linear(128, 6)
+
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.relu(self.bn1(self.conv1(x)))
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x)
+        x = self.layer7(x)
+        x = self.layer8(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+
+        x = self.relu(self.fc_bn1(self.fc1(x)))
+        x = self.dropout(x)
+        x = self.fc2(x)
+
+        return x
+    
+
+class CustomSceneCNN10(nn.Module):
+    def __init__(self):
+        super(CustomSceneCNN10, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.layer1 = ResidualBlock(16, 16)
+        self.layer2 = ResidualBlock(16, 32, stride=2)
+        self.layer3 = ResidualBlock(32, 32)
+        self.layer4 = ResidualBlock(32, 64, stride=2)
+        self.layer5 = ResidualBlock(64, 64)
+        self.layer6 = ResidualBlock(64, 128, stride=2)
+        self.layer7 = ResidualBlock(128, 128)
+        self.layer8 = ResidualBlock(128, 256, stride=2)
+        self.layer9 = ResidualBlock(256, 256)
+        self.layer10 = ResidualBlock(256, 512, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.fc1 = nn.Linear(512, 256)
+        self.fc_bn1 = nn.BatchNorm1d(256)
+        self.fc2 = nn.Linear(256, 6)
+
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.relu(self.bn1(self.conv1(x)))
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x)
+        x = self.layer7(x)
+        x = self.layer8(x)
+        x = self.layer9(x)
+        x = self.layer10(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+
+        x = self.relu(self.fc_bn1(self.fc1(x)))
+        x = self.dropout(x)
+        x = self.fc2(x)
+
+        return x
